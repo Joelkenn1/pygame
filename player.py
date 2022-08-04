92df4c4e62f6821a6326cb2b1b09105db9dcfa98
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-08-04 16:24:13 -0400

    Create player.py

---
+++ player.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("select.png")
+        self.surf = pygame.transform.scale(self.surf, (55, 55))
+        self.rect = self.surf.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.x = x
+        self.y = y
+
+        self.speed = 6
+
+    def update(self, keys):
+        if keys[pygame.K_a] and self.x > 0:
+            # decrement in x co-ordinate
+            self.x -= self.speed
+        # if left arrow key is pressed
+        if keys[pygame.K_d] and self.x < 800:
+            # increment in x co-ordinate
+            self.x += self.speed
+        # if left arrow key is pressed
+        if keys[pygame.K_w] and self.y > 0:
+            # decrement in y co-ordinate
+            self.y -= self.speed
+        # if left arrow key is pressed
+        if keys[pygame.K_s] and self.y < 600:
+            self.y += self.speed
+        self.rect.centerx = round(self.x)
+        self.rect.centery = round(self.y)
